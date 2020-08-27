5545b7f03672e060a149b2501cb267c1508edf2b
Shubham Vijaykumar Patil <shubhampatil9992@gmail.com>
2020-08-27 16:12:51 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -15,16 +15,16 @@ function draw() {
   background(0);  
  if((wall.x-car.x) < (car.width+wall.width)/2)
   {
-    //car.velocityX=0;
+    car.velocityX=0;
     var deformation=(0.5*speed*speed)/22500;
      if(deformation<100){
-      // car.shapeColor=color(0,255,0);
+      car.shapeColor=color(0,255,0);
       }
       if(deformation >100 && deformation <180){
-      //car.shapeColor=color(230,230,0);
+      car.shapeColor=color(230,230,0);
       }
       if(deformation >180){
-       //car.shapeColor=color(250,0,0);
+       car.shapeColor=color(250,0,0);
       }
   }
   drawSprites();
